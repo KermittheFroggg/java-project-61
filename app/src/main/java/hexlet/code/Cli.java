5af523a4845c42c9d3aacd2d7c5f39207c722f65
KermittheFroggg <santamantana@gmail.com>
2023-06-09 15:44:53 +0300

    stage 4

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void Cli() {
+    public static void cli() {
         Scanner name = new Scanner(System.in);
         System.out.printf("May I have your name?");
         String userName = name.next();
